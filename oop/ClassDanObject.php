d3b8c630b78f0cb050304f3a7c85d92fa2109a92
Faris <faris.223040126@mail.unpas.ac.id>
2024-10-10 18:47:26 +0700

    Video 2 - Class dan Object

---
+++ oop/ClassDanObject.php
@@ -0,0 +1,31 @@
+<?php 
+
+class Coba {
+  public $a; //property
+
+  //method
+  public function b(){
+
+  }
+}
+
+// membuat obejct instance dari class
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+
+// Produk
+
+// Jualan Produk
+// Komik
+// Game
+class Produk {
+
+}
+
+$produk1 = new Produk();
+$produk2 = new Produk();
