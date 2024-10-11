59537fda00e807bee1ee9b1b9a34b6e1d6eb433c
Faris <faris.223040126@mail.unpas.ac.id>
2024-10-11 09:14:44 +0700

    Video 13 Abstract Class (Bagian 1)

---
+++ oop/AbstractClass1.php
@@ -0,0 +1,31 @@
+<?php 
+
+abstract class Buah {
+  private $warna;
+
+  abstract public function makan();
+
+  public function setWarna($warna) {
+    $this->warna = $warna;
+  } 
+}
+
+class Apel extends Buah {
+  public function makan() {
+    // kunyah
+    // sampai bagian tengah
+  }
+}
+
+class Jeruk extends Buah {
+  public function makan() {
+    // kupas
+    // kunyah
+  }
+} 
+
+$apel = new Apel();
+$apel->makan();
+
+// $buah = new Buah();
+// $buah->makan();
