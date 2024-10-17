2d02a8808dd1035e37c9b7aad5d716163bf40d3d
Faris <faris.223040126@mail.unpas.ac.id>
2024-10-17 23:01:11 +0700

    Video 5 - View

--- mvc/app/controllers/home.php
+++ mvc/app/controllers/home.php
@@ -1,8 +1,9 @@
-<?php
-class Home
-{
-  public function index()
-  {
-    echo 'home/index';
-  }
+<?php 
+class Home extends Controller{
+    public function index(){
+        $data['judul'] = 'Home';
+        $this->view('templates/header', $data);
+        $this->view('home/index');
+        $this->view('templates/footer');
+    }
 }
