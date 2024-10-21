d6d6f158b04fab09003c8eae34b577fe26bc2180
Faris <faris.223040126@mail.unpas.ac.id>
2024-10-21 13:40:29 +0700

    Video 13 - Searching (revisi)

--- mvc/app/controllers/mahasiswa.php
+++ mvc/app/controllers/mahasiswa.php
@@ -24,7 +24,7 @@ class Mahasiswa extends Controller
     if ($this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0)
     {
       Flasher::setFlash('berhasil', 'ditambahkan', 'success');
-      header('Location : ' . BASEURL . ' /mahasiswa');
+      header('Location: ' . BASEURL . '/mahasiswa');
       exit;
     } else {
       Flasher::setFlash('gagal', 'ditambahkan', 'danger');
